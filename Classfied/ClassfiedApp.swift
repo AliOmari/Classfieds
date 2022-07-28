306f076d68981e49ed5ffd5eab32b4a97a30512c
Ali Al Omari <ali.o@genie-soft.com>
2022-07-28 10:48:56 +0300

    Creating swiftui project with gitignore file.

---
+++ Classfied/ClassfiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassfiedApp.swift
+//  Classfied
+//
+//  Created by Ali Omari on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ClassfiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
